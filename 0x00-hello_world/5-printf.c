b9218ec3590f622fc48154880665f1c251ab4072
ZafCode14 <zafarwp14@gmail.com>
2023-06-15 03:11:24 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - printing text using printf
  * Return: 0
  */
-int main()
+int main(void)
 {
-	printf("ith proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
