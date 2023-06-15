078b62ff1fd062c769579f13bd9c0dde59e1c5e3
ZafCode14 <zafarwp14@gmail.com>
2023-06-15 04:44:50 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+#include<stdio.h>
+/**
+ * main - the entery point
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
