ba70f35424214a7d138136c48243466c2a6e5698
ZafCode14 <zafarwp14@gmail.com>
2023-06-15 03:50:39 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */ 
 int main(void)
 {
-	int charSize = sizeof(char);
-	int intSize = sizeof(int);
-	int longSize = sizeof(long);
-	int longLongSize = sizeof(long long int);
-	int floatSize = sizeof(float);
+	unsigned long charSize = sizeof(char);
+	unsigned long intSize = sizeof(int);
+	unsigned long longSize = sizeof(long);
+	unsigned long longLongSize = sizeof(long long int);
+	unsigned long floatSize = sizeof(float);
 
 	printf("Size of char: %d byte(s)\n", charSize);
 	printf("Size of an int: %d byte(s)\n", intSize);
