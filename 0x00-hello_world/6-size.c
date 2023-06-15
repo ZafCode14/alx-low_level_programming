310d341c6853484c65790728fd2470c378b6aca8
ZafCode14 <zafarwp14@gmail.com>
2023-06-15 03:42:35 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 /**
- * main - printing the sizes of variable types 
+ * main - printing sizes
  * Return: 0
  */
+ 
 int main(void)
 {
 	int charSize = sizeof(char);
@@ -10,6 +11,7 @@ int main(void)
 	int longSize = sizeof(long);
 	int longLongSize = sizeof(long long int);
 	int floatSize = sizeof(float);
+
 	printf("Size of char: %d byte(s)\n", charSize);
 	printf("Size of an int: %d byte(s)\n", intSize);
 	printf("Size of a long int: %d byte(s)\n", longSize);
