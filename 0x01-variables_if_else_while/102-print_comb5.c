21b8b87b8f3afeffbe047f9d1e4d4511df7498e0
ZafCode14 <zafarwp14@gmail.com>
2023-06-16 10:11:49 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,43 @@
 #include<stdio.h>
 /**
- * main - entry point
- * Return: 0
- */
+* main - entry point
+* Return: 0
+*/
 int main(void)
 {
-    int i, j, k, m;
+	int i, j, k, m;
 
-    for (i = 0; i < 100; i++)
-    {
-        k = i / 10;
-        m = i % 10;
-        for (j = i + 1; j < 100; j++)
-        {
-            if (j == 100)
-            {
-                continue;
-            }
-            if (j % 10 == k && j / 10 == m)
-            {
-                continue;
-            }
-            putchar(k + '0');
-            putchar(m + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-            if (i == 98 && j == 99)
-            {
-                putchar('\n');
-            }
-            else
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	for (i = 0; i < 100; i++)
+	{
+		k = i / 10;
+		m = i % 10;
+		for (j = i + 1; j < 100; j++)
+		{
+			if (j == 100)
+			{
+				continue;
+			}
+			if (j % 10 == k && j / 10 == m)
+			{
+				continue;
+			}
+			putchar(k + '0');
+			putchar(m + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 98 && j == 99)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
