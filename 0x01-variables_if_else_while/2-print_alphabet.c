6f411dbfc44e62b9701f0623974361411d9150ce
ZafCode14 <zafarwp14@gmail.com>
2023-06-16 07:52:28 -0700

    1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - itterating for a to z
  * Return: 0
  */
@@ -8,7 +8,8 @@ int main(void)
 {
 	char i;
 
-	for(i = 97; i <= 122; i++){
+	for (i = 97; i <= 122; i++)
+	{
 		putchar(i);
 	}
 	putchar('\n');
