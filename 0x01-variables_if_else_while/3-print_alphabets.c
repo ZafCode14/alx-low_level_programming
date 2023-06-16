e8b28f1f4cd3c9dd88cb44700a71fbc0c96eb8ea
ZafCode14 <zafarwp14@gmail.com>
2023-06-16 07:53:43 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*
+/**
  * main - itterating the alphabet twice
  * Return: 0
  */
@@ -7,10 +7,12 @@ int main(void)
 {
 	char i;
 
-	for(i = 97; i <= 122; i++){
+	for (i = 97; i <= 122; i++)
+	{
 		putchar(i);
 	}
-	for(i = 'A'; i <= 'Z'; i++){
+	for(i = 'A'; i <= 'Z'; i++)
+	{
 		putchar(i);
 	}
 	putchar('\n');
