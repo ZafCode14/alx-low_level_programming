cedc2c3b9e00534f60287600c0baa2a18997f116
ZafCode14 <zafarwp14@gmail.com>
2023-06-16 10:20:59 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,38 +5,25 @@
 */
 int main(void)
 {
-	int i, j, k, m;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
-		k = i / 10;
-		m = i % 10;
 		for (j = i + 1; j < 100; j++)
 		{
-			if (j == 100)
-			{
-				continue;
-			}
-			if (j % 10 == k && j / 10 == m)
-			{
-				continue;
-			}
-			putchar(k + '0');
-			putchar(m + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			if (i == 98 && j == 99)
-			{
-				putchar('\n');
-			}
-			else
+			if (i == 99 || j == 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 
 	return (0);
 }
