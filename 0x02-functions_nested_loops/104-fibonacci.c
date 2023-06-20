953fa0204c5a67fe6e54581500d45429f412c9d3
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 10:26:04 -0700

    16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,20 +4,20 @@
  * Return: 0
  */
 int main() {
-    unsigned long first = 1, second = 2, next;
-    int i;
+	unsigned long first = 1, second = 2, next;
+	int i;
 
-    printf("%lu, %lu, ", first, second); 
+	printf("%lu, %lu, ", first, second); 
 
-    for (i = 3; i <= 93; i++)
+	for (i = 3; i <= 93; i++)
 	{ 
-        next = first + second;
-        printf("%lu, ", next);
-        first = second;
-        second = next;
-    }
+		next = first + second;
+		printf("%lu, ", next);
+		first = second;
+		second = next;
+	}
 
-    printf("%ld\n", 12200160415121876738UL); 
+	printf("%ld\n", 12200160415121876738UL); 
 
-    return 0;
+	return 0;
 }
