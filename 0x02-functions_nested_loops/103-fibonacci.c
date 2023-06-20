89b9a6c175b2a23a9594be3fe311c335355df8be
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 09:31:03 -0700

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - fibonacci to 50
+ * Return: 0
+ */
+int main(void)
+{
+	int current = 2;
+	int previous = 1;
+	int sum = 2;
+	while (current <= 4000000)
+	{
+		int next = current + previous;
+		if (next % 2 == 0)
+			sum += next;
+		previous = current;
+		current = next;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
