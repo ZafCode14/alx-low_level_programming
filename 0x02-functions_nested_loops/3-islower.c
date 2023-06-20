c9dc9eb0437d8e692b09a3b489e7de6470a15880
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 03:20:09 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * _islower - checks if input is lower
+ * c - a variable
  * Return: 0
  */
 int _islower(int c)
 {
-	if (c <= 'a' || c >= 'z')		
-		return 0;
+	if (c <= 'a' || c >= 'z')
+		return (0);
 	else
-		return 1;
+		return (1);
 }
