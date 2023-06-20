69b1fb9c5ccdf4d5e8f9e79de8263ff5c0e59c7e
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 03:52:46 -0700

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,27 +1,12 @@
-#include<stdio.h>
-#include<ctype.h>
+#include"main.h"
 /**
- * main - input function
  * _abs - only positive numbers
+ * @c: recieved character
  * Return: 0
  */
-int _abs(int);
-
-int main(void)
-{
-	int r;
-
-	r = _abs(-1);
-	r = _abs(0);
-	r = _abs(1);
-	r = _abs(-98);
-	
-	return (0);
-}
-
 int _abs(int c)
 {
 	if (c < 0)
 		c = -c;
-	printf("%d\n", c);
+	return (c);
 }
