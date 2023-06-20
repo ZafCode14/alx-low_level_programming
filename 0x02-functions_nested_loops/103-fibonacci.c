a19f992ce1d126b46ef1b81d2453d551bac101a7
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 09:32:08 -0700

    15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,9 +8,11 @@ int main(void)
 	int current = 2;
 	int previous = 1;
 	int sum = 2;
+
 	while (current <= 4000000)
 	{
 		int next = current + previous;
+
 		if (next % 2 == 0)
 			sum += next;
 		previous = current;
