48b09cf2c2ca260256ae38f3df229801fb7901a4
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 03:54:45 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,9 @@
-#include<stdio.h>
+#include "main.h"
 /**
- * main - input function
  * print_last_digit - printing last digit
+ * @c: recieved character
  * Return: 0
  */
-int print_last_digit(int);
-
-int main(void)
-{
-	int r;
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	putchar('0' + r);
-	putchar('\n');
-	return (0);
-}
-
 int print_last_digit(int c)
 {
 	if (c < 0)
