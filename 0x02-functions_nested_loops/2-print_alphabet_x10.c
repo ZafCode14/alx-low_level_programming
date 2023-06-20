85af5f02e89dc65fdf0e071f8a80f276f1731a6f
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 03:11:00 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,8 @@
-#include<stdio.h>
+#include "main.h"
 /**
- * main - input function
  * print_alphabet - printing alpha function x10
  * Return: 0
  */
-void print_alphabet_x10(void);
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
 void print_alphabet_x10(void)
 {
 	int j = 0;
