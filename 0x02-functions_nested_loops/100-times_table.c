72331b27046bb779d42cd23368393af8f2a3ab0b
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 08:27:33 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,15 +5,19 @@
  */
 void print_times_table(int n)
 {
+	int i;
+	int j;
+	int result;
+
 	if (n < 0 || n > 15) {
 		return;
 	}
 
-	for (int i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
-			int result = i * j;
+			result = i * j;
 			if (j == 0) 
 			{
 				_putchar(result + '0');
