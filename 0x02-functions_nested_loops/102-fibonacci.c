87c37547b737ae963b81d715e0866550be11f1f7
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 09:06:40 -0700

    14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	long long int fib[50];
+	long int fib[50];
 	int i;
 
 	fib[0] = 1;
@@ -14,7 +14,7 @@ int main(void)
 		fib[i] = fib[i - 1] + fib[i - 2];
 	for (i = 0; i < 50; i++)
 	{
-		printf("%ld", fib[i]);
+		printf("%lld", fib[i]);
 		if (i < 49)
 			printf(", ");
 	}
