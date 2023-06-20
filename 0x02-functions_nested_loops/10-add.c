24cdbf38e32b3ed5cb2099c383ab16c25b726f9f
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 05:25:55 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,17 +5,6 @@
  * @y: second number
  * Return: 0
  */
-int add(int, int);
-
-int main(void)
-{
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
-}
-
 int add(int x, int y)
 {
 	return (x + y);
