f264f3295f0e8aa230a45972ff23acee6de85062
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 03:44:35 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - checks if input is lower
- * @c - character recieved
+ * @c: character recieved
  * Return: 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))		
-		return 1;
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
 	else
-		return 0;
+		return (0);
 }
