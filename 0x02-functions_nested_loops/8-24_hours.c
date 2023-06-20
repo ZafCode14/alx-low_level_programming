c18e619b73b11b44d5098bf9c6f430c101b0ed1f
ZafCode14 <zafarwp14@gmail.com>
2023-06-20 04:36:12 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * jack_beuer - time function
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int m1;
+	int m2;
+	int h1;
+	int h2;
+
+	for (h2 = 0; h2 < 3; h2++)
+	{
+		for (h1 = 0; h1 < 10; h1++)
+		{
+			for (m2 = 0; m2 < 6; m2++)
+			{
+				for (m1 = 0; m1 < 10; m1++)
+				{
+					putchar(h2 + '0');
+					putchar(h1 + '0');
+					putchar(':');
+					putchar(m2 + '0');
+					putchar(m1 + '0');
+					putchar(' ');
+					putchar('\n');
+				}
+			}
+			if (h2 == 2 && h1 ==3)
+				return;
+		}
+	}
+}
+
