9b60485b85170e925e082a9337d3c5712003ae18
ZafCode14 <zafarwp14@gmail.com>
2023-06-22 07:54:58 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,12 +5,16 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		putchar('-');
+		n = (unsigned int)-n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((unsigned int)n % 10 + '0');
+	else
+		x = (unsigned int)n;
+	if (x / 10 != 0)
+		print_number(x / 10);
+	putchar(x % 10 + '0');
 }
