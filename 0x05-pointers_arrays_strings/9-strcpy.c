cc9643a80d66d37424c2992df20e2d07c28d6007
ZafCode14 <zafarwp14@gmail.com>
2023-06-26 07:17:51 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _strcpy - copy string
+ * @dest: pointer to dest
+ * @src: pointer to src
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int i;
+
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
