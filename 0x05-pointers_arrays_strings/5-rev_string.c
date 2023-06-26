c8daa2f4ec36b33c477444e5584d5edff919021b
ZafCode14 <zafarwp14@gmail.com>
2023-06-26 04:32:41 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer to string
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	char temp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	char *end = &*(s + len);
+
+	while (s < end)
+	{
+		temp = *s;
+		*s = *end;
+		*end = temp;
+		s++;
+		end--;
+	}
+}
