664f95532b5ea93bfa5d8a84438816eb561e7e54
ZafCode14 <zafarwp14@gmail.com>
2023-06-26 06:54:35 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i == n - 1)
-			printf("\n");
-		else
+		if (i < n - 1)
 			printf(", ");
 	}
+	printf("\n");
 }
