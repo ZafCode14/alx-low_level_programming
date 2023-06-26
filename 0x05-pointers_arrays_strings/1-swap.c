14ea5d31a7d11e391d47ffb8d67bcbf520385dcc
ZafCode14 <zafarwp14@gmail.com>
2023-06-26 02:17:25 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * swap_int - swapping using pointers
+ * @a: pointer a.
+ * @b: pointer b.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
