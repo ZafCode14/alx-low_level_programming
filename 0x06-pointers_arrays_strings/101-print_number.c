584598b457b8852c1789f504ef8ad238eab9a867
ZafCode14 <zafarwp14@gmail.com>
2023-06-29 05:31:35 -0700

    9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,15 +5,16 @@
  */
 void print_number(int n)
 {
-	n = (unsigned int)n;
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = (unsigned int)-n;
 	}
-	if (n / 10 > 0)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	else
+		x = (unsigned int)n;
+	if (x / 10 != 0)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
