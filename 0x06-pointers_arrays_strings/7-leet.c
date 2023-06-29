34ed3f402f96a2e644ebe60c58a59a6c75d144aa
ZafCode14 <zafarwp14@gmail.com>
2023-06-29 04:24:30 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,13 +11,13 @@ char *leet(char *a)
 	{
 		if (*a == 'a' || *a == 'A')
 			*a = '4';
-		else if (*a == 'e' || *a == 'E')
+		if (*a == 'e' || *a == 'E')
 			*a = '3';
-		else if (*a == 'o' || *a == 'O')
+		if (*a == 'o' || *a == 'O')
 			*a = '0';
-		else if (*a == 't' || *a == 'T')
+		if (*a == 't' || *a == 'T')
 			*a = '7';
-		else if (*a == 'l' || *a == 'L')
+		if (*a == 'l' || *a == 'L')
 			*a = '1';
 		a++;
 	}
