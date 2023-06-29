e80e33a364980416ed2a1d278ce1210ce86af7d8
ZafCode14 <zafarwp14@gmail.com>
2023-06-29 03:12:02 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,9 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' || *s2 != '\0')
 	{
 		if (*s1 < *s2)
-			return (-15);
+			return (*s1 - *s2);
 		else if (*s1 > *s2)
-			return (15);
+			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
