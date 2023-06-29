f0ba752a62b1e6a8bfce6ad8b7e1201a206f883c
ZafCode14 <zafarwp14@gmail.com>
2023-06-29 03:05:20 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,15 +2,16 @@
  * _strcmp - comparing two strings
  * @s1: pointer to first string
  * @s2: pointer to second string
+ * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' || *s2 != '\0')
 	{
 		if (*s1 < *s2)
-			return -1;
+			return (-1);
 		else if (*s1 > *s2)
-			return 1;
+			return (1);
 		s1++;
 		s2++;
 	}
