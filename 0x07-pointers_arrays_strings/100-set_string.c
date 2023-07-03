faebfa4bbd522e1d50023b728e7266d2ce3495b1
ZafCode14 <zafarwp14@gmail.com>
2023-07-03 04:48:22 -0700

    100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,33 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+/**
+ * _strlen - string length
+ * @s: pointer to string
+ * Return: p - s
+ */
+size_t _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p != '\0')
+		p++;
+	return (p - a);
+}
+/**
+ * _strcpy - string copy
+ * @dest: pointer to string1
+ * @src: pointer to string2
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*src != '\0')
+		*p++ = *src++;
+	*p = '\0';
+	return (dest);
+}
 /**
  * set_string - set value of pointer to a character
  * @s: pointer to pointer to string
@@ -8,6 +35,15 @@
  */
 void set_string(char **s, char *to)
 {
-	*s = (char *) malloc((strlen(to) + 1) * sizeof(char));
-	strcpy(*s, to);
+	size_t len = _strlen(to);
+
+	if (len == 0)
+	{
+		*s = NULL;
+		return;
+	}
+	char tmp[len + 1];
+
+	_strcpy(tmp, to);
+	*s = tmp;
 }
