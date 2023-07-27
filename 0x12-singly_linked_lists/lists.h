abd18e802338583d8f8a10d1aad0c4687479999a
ZafCode14 <zafarwp14@gmail.com>
2023-07-27 06:08:34 -0700

    betty lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LISTS_H 
+#define LISTS_H
 
 #include <stddef.h>
 #include <stdio.h>
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -26,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */ 
+#endif /* LISTS_H */
