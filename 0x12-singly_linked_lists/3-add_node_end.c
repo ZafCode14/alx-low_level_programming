313835d7456fd45740eeef9da6ea067fef2e5d9b
ZafCode14 <zafarwp14@gmail.com>
2023-07-27 06:03:27 -0700

    tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,36 +9,36 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
- 	list_t *new, *temp;
+	list_t *new, *temp;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new->len = strlen(str);
-    new->next = NULL;
+	new->len = strlen(str);
+	new->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    temp->next = new;
+	temp->next = new;
 
-    return (new);
+	return (new);
 }
