bb13cc9157ae6432c6c396c44044098a5514a604
ZafCode14 <zafarwp14@gmail.com>
2023-08-02 03:24:18 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,31 @@
 #include "lists.h"
 /**
+ * looped_listint_len - returns the number of nodes in a looped linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: the number of nodes in the looped list, or 0 if there is no loop
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *slow = head, *fast = head;
+	size_t count = 2;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			do {
+				count++;
+				fast = fast->next;
+			} while (slow != fast);
+			return (count);
+		}
+	}
+	return (0);
+}
+/**
  * print_listint_safe - prints a linked list
  * @head: pointer to head
  *
@@ -7,24 +33,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *current = head;
-	const listint_t *next = NULL;
+	size_t nodes, index = 0;
 
-	while (current != NULL)
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)current, current->n);
-		next = current->next;
-
-		if (next >= current)
+		for (; head != NULL; nodes++)
 		{
-			printf("-> [%p] %d\n", (void *)next, next->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-
-		current = next;
 	}
-
-	return (count);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (nodes);
 }
