24b635a2fa197bc72bb3ac8b5fc7fcaa9dcf995a
ZafCode14 <zafarwp14@gmail.com>
2023-08-08 11:23:55 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_written;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	len = strlen(text_content);
-	bytes_written = write(fd, text_content, len);
-	if (bytes_written != len)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		len = strlen(text_content);
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
