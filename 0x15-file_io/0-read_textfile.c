9c5c21ef130bbe7e0e55ecf1b846830c6ad8bb9f
ZafCode14 <zafarwp14@gmail.com>
2023-08-08 04:24:50 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: pointer to the file name
@@ -12,9 +10,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *file;
-	char *buffer;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
+	char buffer[1024];
+	ssize_t len, bytes_written;
 
 	if (filename == NULL)
 		return (0);
@@ -23,30 +20,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (file == NULL)
 		return (0);
 
-	buffer = (char *)malloc(letters + 1);
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return (0);
-	}
+	len = fread(buffer, sizeof(char), letters, file);
+	buffer[len] = '\0';
 
-	bytes_read = fread(buffer, sizeof(char), letters, file);
+	bytes_written = write(STDOUT_FILENO, buffer, len);
+
 	fclose(file);
 
-	if (bytes_read == 0)
-	{
-		free(buffer);
+	if (bytes_written != len)
 		return (0);
-	}
-
-	buffer[bytes_read] = '\0';
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	free(buffer);
-
-	if (bytes_written != bytes_read)
-		return (0);
-
 	return (bytes_written);
 }
