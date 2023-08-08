023beb4d9ee46d1e0e437ccee6e9a48b2b0f47f7
ZafCode14 <zafarwp14@gmail.com>
2023-08-08 11:19:49 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
@@ -18,22 +14,16 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_written;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content != NULL)
+	len = strlen(text_content);
+	bytes_written = write(fd, text_content, len);
+	if (bytes_written != len)
 	{
-		len = strlen(text_content);
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written != len)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
