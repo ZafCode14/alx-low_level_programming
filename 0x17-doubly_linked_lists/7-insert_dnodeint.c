c48e95f6b3373914d3232d210334919603f705ee
Mohamed El-Zafarani <misha@MSI>
2023-09-14 14:21:16 +0200

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,43 +9,37 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp, *temp2, *temp3;
-	unsigned int count = 0;
+	dlistint_t *new, *current;
+	unsigned int i = 0;
 
-	for (temp3 = *h; temp3->next; count++)
-		temp3 = temp3->next;
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	current = *h;
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	temp = *h;
-	if (idx == 0)
-	{
-		new->next = *h;
-		new->prev = NULL;
-		*h = new;
-	}
-	else if (idx == count + 1)
-	{
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new;
-		new->next = NULL;
-		new->prev = temp;
-	}
-	else if (idx > 0 && idx < count + 1)
-	{
-		for (; idx != 0; idx--)
-			temp = temp->next;
-		temp2 = temp->prev;
-		temp2->next = new;
-		new->prev = temp2;
-		new->next = temp;
-		temp->prev = new;
-	}
-	else if (idx > count + 1)
-		return (NULL);
+	new->prev = current;
+	new->next = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = new;
+
+	current->next = new;
+
 	return (new);
 }
