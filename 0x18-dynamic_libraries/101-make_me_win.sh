6c7dc6b18eef5415ec183dacfde3b260786e2fca
Mohamed El-Zafarani <misha@MSI>
2023-09-18 16:45:00 +0200

    101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ZafCode14/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib.so
-export LD_PRELOAD=/tmp/lib.so
+wget -P /tmp https://github.com/ZafCode14/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/tmp/libwin.so
