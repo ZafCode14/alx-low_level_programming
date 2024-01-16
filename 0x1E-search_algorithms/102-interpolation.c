d55ba622c913f5be4b8dc9ba016b63ea02a1258f
ZafCode14 <zafarwp14@gmail.com>
2024-01-16 15:45:20 +0200

    102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,7 +17,7 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (low < pos || high > pos)
+	while (low <= high)
 	{
 		pos = low + (((double)(high - low) /
 					(array[high] - array[low])) * (value - array[low]));
