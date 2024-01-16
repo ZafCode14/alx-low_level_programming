e4f61b38fd3df61e8b8b9fbed1664b07da141896
ZafCode14 <zafarwp14@gmail.com>
2024-01-16 15:36:48 +0200

    100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 	int step = (int)sqrt(size);
 	int low, high;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (array[i] < value && i < (int)size)
