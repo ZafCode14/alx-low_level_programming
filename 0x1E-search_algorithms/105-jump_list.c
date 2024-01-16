994ccf122bdd528b8214259422b475beb13988e9
ZafCode14 <zafarwp14@gmail.com>
2024-01-16 16:58:28 +0200

    105

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,38 +11,29 @@
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	listint_t *temp = list;
-	listint_t *temp2 = list;
-	size_t step = sqrt(size), from, to, i;
+	listint_t *prev;
+	size_t step = sqrt(size), i;
 
 	if (list == NULL)
 		return (NULL);
 
 	while (temp->n < value && temp->index < size - 1)
 	{
+		prev = temp;
 		for (i = 0; i < step && temp->index < size - 1; i++)
 			temp = temp->next;
 		printf("Value checked at index[%ld] = [%d]\n", temp->index, temp->n);
 	}
 
-	for (from = 0; from < temp->index; from += step)
-		;
-	from -= step;
-	to = temp->index;
-	printf("Value found between indexes [%ld] and [%ld]\n", from, to);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			prev->index, temp->index);
 
-	while (temp2->index < from)
-		temp2 = temp2->next;
-
-	while (temp2->index <= to)
+	while (prev != NULL)
 	{
-		printf("Value checked at index[%ld] = [%d]\n", temp2->index, temp2->n);
-		if (temp2->n == value)
-			return (temp2);
-
-		if (temp2->index < to)
-			temp2 = temp2->next;
-		else
-			break;
+		printf("Value checked at index[%ld] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
 	}
 	return (NULL);
 }
